414d8b6915e8cb1375574218e678fc1d128e3629
Felix Schwarz <felix.schwarz@schwarz.eu>
2008-11-23 11:28:18 +0100

    added license boiler plates

--- pymta/session.py
+++ pymta/session.py
@@ -1,4 +1,26 @@
 # -*- coding: UTF-8 -*-
+#
+# The MIT License
+# 
+# Copyright (c) 2008 Felix Schwarz <felix.schwarz@oss.schwarz.eu>
+# 
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+# 
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
+# 
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+# THE SOFTWARE.
 
 import re
 from sets import Set
@@ -7,16 +29,17 @@ from repoze.workflow.statemachine import StateMachine, StateMachineError
 
 from pymta.model import Message, Peer
 
+
 __all__ = ['SMTPSession']
-
-
-class InvalidParametersException(Exception):
-    pass
 
 
 # regular expression deliberately taken from
 # http://stackoverflow.com/questions/106179/regular-expression-to-match-hostname-or-ip-address#106223
 regex_string = r'^(([a-zA-Z0-9]|[a-zA-Z0-9][a-zA-Z0-9\-]*[a-zA-Z0-9])\.)*([A-Za-z]|[A-Za-z][A-Za-z0-9\-]*[A-Za-z0-9])$'
+
+
+class InvalidParametersException(Exception):
+    pass
 
 
 class SMTPSession(object):
@@ -35,9 +58,7 @@ class SMTPSession(object):
         self._command_arguments = None
         self._message = None
         
-        
         self.hostname_regex = re.compile(regex_string, re.IGNORECASE)
-        
         self._build_state_machine()
         
     
