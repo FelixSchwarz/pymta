47ad4d2f6ac9228a65d6bb73659103346f0ffcf5
Felix Schwarz <felix.schwarz@schwarz.eu>
2008-11-23 11:34:41 +0100

    removed dependency on Python's smtpd

--- pymta/mta.py
+++ pymta/mta.py
@@ -1,5 +1,28 @@
 # -*- coding: UTF-8 -*-
+# 
+# The MIT License
+# 
+# Copyright (c) 2008 Felix Schwarz <felix.schwarz@oss.schwarz.eu>
+# 
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+# 
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
+# 
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+# THE SOFTWARE.
 
+import asyncore
 import socket
 
 from pymta.smtpd import SMTPServer
@@ -8,13 +31,23 @@ from pymta.command_parser import SMTPCommandParser
 __all__ = ['PythonMTA']
 
 
-class PythonMTA(SMTPServer):
+class PythonMTA(asyncore.dispatcher):
     version='0.1'
 
     def __init__(self, localaddr, remoteaddr, policy_class):
-        SMTPServer.__init__(self, localaddr, remoteaddr)
+        asyncore.dispatcher.__init__(self)
+        self._localaddr = localaddr
+        self._remoteaddr = remoteaddr
         self._policy_class = policy_class
+        
         self._primary_hostname = socket.getfqdn()
+        
+        # Copied from Python's smtpd
+        self.create_socket(socket.AF_INET, socket.SOCK_STREAM)
+        # try to re-use a server port if possible
+        self.set_reuse_addr()
+        self.bind(localaddr)
+        self.listen(5)        
     
     
     def handle_accept(self):
