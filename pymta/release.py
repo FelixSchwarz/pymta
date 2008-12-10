d40c29d9a9d60f66587ec0e5f15b5d6479f66628
Felix Schwarz <felix.schwarz@schwarz.eu>
2008-12-10 19:28:42 +0100

    added setuptools integration

---
+++ pymta/release.py
@@ -0,0 +1,20 @@
+# -*- coding: UTF-8 -*-
+"""Release information about pymta."""
+
+name = "pymta"
+version = "0.2"
+description = "pytma provides a basis for build an SMTP server with customized behavior"
+long_description = """pymta is a library to build a custom SMTP server in Python. This is useful if 
+you want to...
+ * test mail-sending code against a real SMTP server even in your unit tests.
+ * build a custom SMTP server with non-standard behavior without reimplementing 
+   the whole SMTP protocol.
+ * have a low-volume SMTP server which can be easily extended using Python
+"""
+author = "Felix Schwarz"
+email = "felix.schwarz@oss.schwarz.eu"
+url = "http://oss.schwarz.eu/cgi-bin/hgwebdir.cg/pymta"
+download_url = "http://oss.schwarz.eu/cgi-bin/hgwebdir.cg/pymta"
+copyright = "© 2008 Felix Schwarz"
+license="MIT"
+
---
+++ setup.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python
+# -*- coding: UTF-8 -*-
+
+import os
+from setuptools import setup
+
+
+execfile(os.path.join("pymta", "release.py"))
+
+setup(
+      name="pymta",
+      version=version,
+        
+      description=description,
+      long_description=long_description,
+      author=author,
+      author_email=email,
+      url=url,
+      download_url=download_url,
+      license=license,
+        
+      install_requires=['repoze.workflow'],
+      zip_safe=True,
+      packages=['pymta'],
+      classifiers = [
+          'Development Status :: 3 - Development/Alpha',
+          'Intended Audience :: Developers',
+          'License :: OSI Approved :: MIT License',
+          'Operating System :: OS Independent',
+          'Programming Language :: Python',
+          'Topic :: Communications :: Email',
+          'Topic :: Software Development :: Libraries :: Python Modules',
+        ],
+        test_suite = 'nose.collector',
+)
+
+
