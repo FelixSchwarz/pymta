02d59eb05898bb6c8a79d8337cea2cbcb63a3ee9
Felix Schwarz <felix.schwarz@schwarz.eu>
2009-02-28 13:40:23 +0100

    refactored the source dist assembling script so that name/version are not hard coded any more and the file list is separated more from the real algorithm

--- assemble_source_release.py
+++ assemble_source_release.py
@@ -5,11 +5,15 @@ import os
 from StringIO import StringIO
 import subprocess
 import tarfile
+import tempfile
 
 
 def build_documentation(project_dir):
     doc_dir = os.path.join(project_dir, 'docs')
-    subprocess.call(['make', 'html'], cwd=doc_dir)
+    temp_file, temp_name = tempfile.mkstemp()
+    # We don't need any output on stdout
+    subprocess.call(['make', 'html'], cwd=doc_dir, stdout=temp_file)
+    os.unlink(temp_name)
 
 def make_relative_filename(topdir, filename):
     assert filename.startswith(topdir)
@@ -23,41 +27,66 @@ def make_tarname(topdir, filename, path_prefix):
     tarname = '%s/%s' % (path_prefix, relative_name)
     return tarname
 
-def create_tarball(project_dir, path_prefix):
+def add_file(tar, filename, arcname, project_dir, path_prefix):
+    tarname = make_tarname(project_dir, filename, path_prefix)
+    if arcname is not None:
+        tarname = make_tarname(project_dir, arcname, path_prefix)
+    tar.add(filename, arcname=tarname)
+
+def build_fname_with_changed_path_prefix(project_dir, root, arcname, basename):
+    nr_of_dirs = lambda path: len(path.split('/'))
+    relative_root = make_relative_filename(project_dir, root)
+    assert nr_of_dirs(arcname) <= nr_of_dirs(relative_root), 'Untested'
+    
+    offset_path_items = relative_root.split('/')[nr_of_dirs(arcname):]
+    offset_path = os.path.join(arcname, *offset_path_items)
+    tar_fname = os.path.join(project_dir, offset_path, basename)
+    return tar_fname
+
+def add_files_below_directory(tar, dirname, arcname, project_dir, path_prefix):
+    for (root, dirs, files) in os.walk(dirname):
+        for basename in files:
+            if basename.endswith('.pyc'):
+                continue
+            fname = os.path.join(root, basename)
+            tar_fname = fname
+            if arcname is not None:
+                tar_fname = build_fname_with_changed_path_prefix(project_dir, root, arcname, basename)
+            tarname = make_tarname(project_dir, tar_fname, path_prefix)
+            tar.add(fname, tarname)
+
+def create_tarball(project_dir, package_files, path_prefix):
     tar_fp = StringIO()
     tar = tarfile.open(fileobj=tar_fp, mode='w')
     
-    filenames = ['docs', 'examples', 'pymta', 'tests', 'Changelog.txt', 'COPYING.txt', 'setup.py']
-    for filename in filenames:
+    for filename in package_files:
+        arcname = None
+        if not isinstance(filename, basestring):
+            filename, arcname = filename
         filename = os.path.join(project_dir, filename)
         if os.path.isfile(filename):
-            tarname = make_tarname(project_dir, filename, path_prefix)
-            tar.add(filename, arcname=tarname)
+            add_file(tar, filename, arcname, project_dir, path_prefix)
         else:
-            for (root, dirs, files) in os.walk(filename):
-                for dirname in dirs:
-                    tarname = make_tarname(project_dir, dirname, path_prefix)
-                    tar.add(dirname, arcname=tarname)
-                for fname in files:
-                    if not fname.endswith('.pyc'):
-                        fname = os.path.join(root, fname)
-                        tarname = make_tarname(project_dir, fname, path_prefix)
-                        tar.add(fname, tarname)
-    tar.add('build/html', arcname='%s/docs/html' % path_prefix)
+            add_files_below_directory(tar, filename, arcname, project_dir, path_prefix)
     tar.close()
     tar_fp.seek(0,0)
     return tar_fp
 
+def get_name_and_version():
+    release_info = {}
+    execfile(os.path.join("pymta", "release.py"), release_info)
+    return (release_info['name'], release_info['version'])
+
 def main():
-    # TODO: Get this from release.py
-    name = 'pymta'
-    release = '0.3.1'
-    
+    name, version = get_name_and_version()
     this_dir = os.path.abspath(os.path.dirname(__file__))
     build_documentation(this_dir)
-    tar_fp = create_tarball(this_dir, '%s-%s' % (name, release))
     
-    gz_filename = '%s-%s.tar.gz' % (name, release)
+    package_files = ('docs', 'examples', 'pymta', 'tests', 'Changelog.txt', 
+                     'COPYING.txt', 'setup.py', ('build/html', 'docs/html'))
+    tar_fp = create_tarball(this_dir, package_files, '%s-%s' % (name, version))
+    
+    gz_filename = '%s-%s.tar.gz' % (name, version)
     gzip.open(gz_filename, 'wb').write(tar_fp.read())
 
 if __name__ == '__main__':
