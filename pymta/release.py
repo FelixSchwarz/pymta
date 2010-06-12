49940a890448d2a4b2f213329ec82262f2e0df0c
Felix Schwarz <felix.schwarz@oss.schwarz.eu>
2010-06-12 15:02:38 +0200

    back to development mode

--- pymta/release.py
+++ pymta/release.py
@@ -2,7 +2,7 @@
 "Release information about pymta."
 
 name = 'pymta'
-version = '0.5.2'
+version = '0.6dev'
 description = 'library to build a custom SMTP server'
 long_description = '''
 pymta is a library to build a custom SMTP server in Python. This is useful if 
