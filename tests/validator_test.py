392c5b8ee87f56bcddb2962dbb5da6aa17771d5b
Felix Schwarz <felix.schwarz@oss.schwarz.eu>
2024-07-16 22:31:15 +0200

    validator_test.py: use plain pytest functions

--- tests/validator_test.py
+++ tests/validator_test.py
@@ -3,34 +3,30 @@
 
 from __future__ import print_function, unicode_literals
 
-from unittest import TestCase
-
 from pycerberus.errors import InvalidDataError
 import pytest
 
 from pymta.validation import SMTPEmailValidator
 
 
-class SMTPEmailValidatorTest(TestCase):
+def _process(input_string):
+    return SMTPEmailValidator().process(input_string)
 
-    def process(self, input_string):
-        return SMTPEmailValidator().process(input_string)
+def test_accept_plain_email_address():
+    assert _process('foo@example.com') == 'foo@example.com'
 
-    def test_accept_plain_email_address(self):
-        assert self.process('foo@example.com') == 'foo@example.com'
+def test_accept_email_address_in_angle_brackets():
+    assert _process('<foo@example.com>') == 'foo@example.com'
 
-    def test_accept_email_address_in_angle_brackets(self):
-        assert self.process('<foo@example.com>') == 'foo@example.com'
+def test_reject_email_address_in_unbalanced_angle_brackets():
+    with pytest.raises(InvalidDataError):
+        _process('<foo@example.com')
+    with pytest.raises(InvalidDataError):
+        _process('foo@example.com>')
 
-    def test_reject_email_address_in_unbalanced_angle_brackets(self):
-        with pytest.raises(InvalidDataError):
-            self.process('<foo@example.com')
-        with pytest.raises(InvalidDataError):
-            self.process('foo@example.com>')
-
-    def test_reject_invalid_email_addresses(self):
-        with pytest.raises(InvalidDataError):
-            self.process('foo@@example.com')
-        with pytest.raises(InvalidDataError):
-            self.process('foo@example..com')
+def test_reject_invalid_email_addresses():
+    with pytest.raises(InvalidDataError):
+        _process('foo@@example.com')
+    with pytest.raises(InvalidDataError):
+        _process('foo@example..com')
 
